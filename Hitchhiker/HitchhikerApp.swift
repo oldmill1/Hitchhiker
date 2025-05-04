35741b4c0a2a50a1a9d235d778566aea423605e0
Ankur Taxali <ataxali@gmail.com>
2025-05-03 20:39:23 -0400

    Initial Commit

---
+++ Hitchhiker/HitchhikerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HitchhikerApp.swift
+//  Hitchhiker
+//
+//  Created by Ankur Taxali on 2025-05-03.
+//
+
+import SwiftUI
+
+@main
+struct HitchhikerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HitchhikerTests/HitchhikerTests.swift
@@ -0,0 +1,17 @@
+//
+//  HitchhikerTests.swift
+//  HitchhikerTests
+//
+//  Created by Ankur Taxali on 2025-05-03.
+//
+
+import Testing
+@testable import Hitchhiker
+
+struct HitchhikerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
