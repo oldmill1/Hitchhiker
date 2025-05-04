892b60a232a29b1ec6c86c389340be1aecd2fa68
Ankur Taxali <ataxali@gmail.com>
2025-05-03 21:24:35 -0400

    save

--- Hitchhiker/Views/DayView.swift
+++ Hitchhiker/Views/DayView.swift
@@ -7,14 +7,30 @@ struct DayView: View {
     let movements: [Movement]
 
     var body: some View {
-        List {
-            Section(header: Text(day.capitalized).font(.largeTitle)) {
-                ForEach(movements) { movement in
-                    Text(movement.name)
+        VStack(spacing: 0) {
+            Button(action: {
+                // Play button action goes here
+                print("Play tapped")
+            }) {
+                Text("Play")
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .padding([.horizontal, .top])
+            }
+
+            List {
+                Section(header: Text(day.capitalized).font(.largeTitle)) {
+                    ForEach(movements) { movement in
+                        Text(movement.name)
+                    }
                 }
             }
+            .listStyle(.insetGrouped)
         }
-        .listStyle(.insetGrouped)
     }
 }
 
@@ -23,3 +39,4 @@ struct DayView: View {
         DayView(day: "Monday", movements: MovementData.mondayMovements)
     }
 }
+
