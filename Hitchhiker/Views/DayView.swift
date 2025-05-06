02d2108d6d3fe7ff897e29bcf13afa641abfaaa8
Ankur Taxali <ataxali@gmail.com>
2025-05-06 14:07:11 -0400

    save

--- Hitchhiker/Views/DayView.swift
+++ Hitchhiker/Views/DayView.swift
@@ -30,52 +30,7 @@ struct DayView: View {
         VStack(spacing: 0) {
             DisplayView()
 
-            ControlsView(
-                currentMovement: currentMovement,
-                isInPause: isInPause,
-                timeRemaining: timeRemaining,
-                isPlaying: isPlaying,
-                onPlayPauseTapped: {
-                    isPlaying.toggle()
-                    
-                    if isPlaying {
-                        if timeRemaining == 0, let set = currentSet {
-                            timeRemaining = isInPause ? set.pauseBetween : set.duration
-                        }
-                        startTimer()
-                    } else {
-                        stopTimer()
-                    }
-                }
-            )
-            
-            PlaylistView(
-                movementSets: movementSets,
-                currentMovement: currentMovement,
-                expandedSections: expandedSections,
-                onToggleSection: { id in
-                    if expandedSections.contains(id) {
-                        expandedSections.remove(id)
-                    } else {
-                        expandedSections.insert(id)
-                    }
-                },
-                onSelectMovement: { setId, index in
-                    if let tappedSetIndex = movementSets.firstIndex(where: { $0.id == setId }) {
-                        currentSetIndex = tappedSetIndex
-                        currentMovementIndex = index
-                        isInPause = false
-                        isPlaying = true
-
-                        if let newSet = currentSet {
-                            timeRemaining = newSet.duration
-                        }
-
-                        startTimer()
-                    }
-                }
-            )
-                .frame(maxHeight: .infinity)    
+            Spacer()
         }
         
     }
--- Hitchhiker/Views/DisplayView.swift
+++ Hitchhiker/Views/DisplayView.swift
@@ -4,10 +4,9 @@ struct DisplayView: View {
     var body: some View {
         Image("headTurnLeftRight")
             .resizable()
-            .aspectRatio(contentMode: .fill)
-            .clipped()
-            .padding(.top, 61)
-            .offset(x: -40)
+            .aspectRatio(contentMode: .fill) // use `.fill` to stretch width
+            .frame(height: 290)
+            .clipped() // trims overflow to prevent distortion
             .ignoresSafeArea(edges: .top)
     }
 }
